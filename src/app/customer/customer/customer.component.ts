1f00def11b3bda391a888eb1c9d679ef166e1f79
HoangLong <hoanglongwork@gmail.com>
2019-04-23 11:00:09 +0700

    done index page

--- src/app/customer/customer/customer.component.ts
+++ src/app/customer/customer/customer.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import { Renderer2 } from '@angular/core';
+import { MatAutocompleteTrigger } from '@angular/material';
 
 @Component({
   selector: 'app-customer',
@@ -9,25 +11,60 @@ import {map, startWith} from 'rxjs/operators';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
+  @ViewChild('inputFrom', {read: MatAutocompleteTrigger}) autoCompleteInputFrom: MatAutocompleteTrigger;
+  @ViewChild('inputTo', {read: MatAutocompleteTrigger}) autoCompleteInputTo: MatAutocompleteTrigger;
   myControl = new FormControl();
+  myControlTo = new FormControl();
   options: string[] = ['Hồ Chí Minh (SNG)', 'Hà Nội (HN)', 'Đà Nẵng (DN)'];
   filteredOptions: Observable<string[]>;
+  filteredOptionsTo: Observable<string[]>;
+  fromValue: string = '';
+  toValue: string = '';
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
 
   }
 
   ngOnInit() {
+    this.fromValue = this.options[0];
+    this.toValue = this.options[1];
+    this.myControl.setValue(this.fromValue);
+    this.myControlTo.setValue(this.toValue);
     this.filteredOptions = this.myControl.valueChanges
     .pipe(
       startWith(''),
-      map(value => this._filter(value))
+      map(value => {
+        if ( value !== '' ) {
+          this.fromValue = value;
+        }
+        return this._filter(value)
+      })
+    );
+    this.filteredOptionsTo = this.myControlTo.valueChanges
+    .pipe(
+      startWith(''),
+      map(value => {
+        if ( value !== '' ) {
+          this.toValue = value;
+          // this.renderer.selectRootElement('#inputFrom').focus();
+        }
+        return this._filter(value)
+      })
     );
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  rotateFromTo() {
+    const temp = this.fromValue;
+    this.fromValue = this.toValue;
+    this.toValue = temp;
+
+    this.myControl.setValue(this.fromValue);
+    this.myControlTo.setValue(this.toValue);
   }
 
   myFilter = (pickedDate: Date): boolean => {
@@ -38,5 +75,14 @@ export class CustomerComponent implements OnInit {
     } else {
       return true;
     }
+  }
+
+  clearInputTo(myControlTo: FormControl){
+    myControlTo.setValue('');
+    this.autoCompleteInputTo.openPanel();
+  }
+  clearInputFrom(myControl: FormControl){
+    myControl.setValue('');
+    this.autoCompleteInputFrom.openPanel();
   }
 }
