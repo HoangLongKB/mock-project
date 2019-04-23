d7b2184a8e9236fe83f424cc08c28d875946ba2a
HoangLong <hoanglongwork@gmail.com>
2019-04-23 17:01:02 +0700

    validate date depature and return date, still need to fix input place validate

--- src/app/customer/customer/customer.component.ts
+++ src/app/customer/customer/customer.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-import { Renderer2 } from '@angular/core';
-import { MatAutocompleteTrigger } from '@angular/material';
 
 @Component({
   selector: 'app-customer',
@@ -11,25 +9,22 @@ import { MatAutocompleteTrigger } from '@angular/material';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
-  @ViewChild('inputFrom', {read: MatAutocompleteTrigger}) autoCompleteInputFrom: MatAutocompleteTrigger;
-  @ViewChild('inputTo', {read: MatAutocompleteTrigger}) autoCompleteInputTo: MatAutocompleteTrigger;
-  myControl = new FormControl();
-  myControlTo = new FormControl();
   options: string[] = ['Hồ Chí Minh (SNG)', 'Hà Nội (HN)', 'Đà Nẵng (DN)'];
+  myControl = new FormControl(this.options[0]);
+  myControlTo = new FormControl(this.options[1]);
+  dateControl = new FormControl(new Date());
+  dateControlReturn = new FormControl(new Date());
   filteredOptions: Observable<string[]>;
   filteredOptionsTo: Observable<string[]>;
   fromValue: string = '';
   toValue: string = '';
 
-  constructor(private renderer: Renderer2) {
-
+  constructor() {
   }
 
   ngOnInit() {
     this.fromValue = this.options[0];
     this.toValue = this.options[1];
-    this.myControl.setValue(this.fromValue);
-    this.myControlTo.setValue(this.toValue);
     this.filteredOptions = this.myControl.valueChanges
     .pipe(
       startWith(''),
@@ -37,7 +32,7 @@ export class CustomerComponent implements OnInit {
         if ( value !== '' ) {
           this.fromValue = value;
         }
-        return this._filter(value)
+        return this._filter(value);
       })
     );
     this.filteredOptionsTo = this.myControlTo.valueChanges
@@ -46,9 +41,8 @@ export class CustomerComponent implements OnInit {
       map(value => {
         if ( value !== '' ) {
           this.toValue = value;
-          // this.renderer.selectRootElement('#inputFrom').focus();
         }
-        return this._filter(value)
+        return this._filter(value);
       })
     );
   }
@@ -70,19 +64,31 @@ export class CustomerComponent implements OnInit {
   myFilter = (pickedDate: Date): boolean => {
     const now = new Date();
     now.setHours(0, 0, 0, 0);
-    if( pickedDate < now ) {
+    if ( pickedDate <= now) {
       return false;
     } else {
       return true;
     }
   }
+  myFilter2 = (pickedDate: Date): boolean => {
+    const now = new Date();
+    now.setHours(0, 0, 0, 0);
+    if ( pickedDate <= now || pickedDate < this.dateControl.value) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  getDateChange(event) {
+    if (event.value > this.dateControlReturn.value) {
+      this.dateControlReturn.setValue(event.value);
+    }
+  }
 
   clearInputTo(myControlTo: FormControl){
     myControlTo.setValue('');
-    this.autoCompleteInputTo.openPanel();
   }
   clearInputFrom(myControl: FormControl){
     myControl.setValue('');
-    this.autoCompleteInputFrom.openPanel();
   }
 }
